767544a2847bd7fc9d8e42eeecab9efa6348f600
mxzf <mxzf42@gmail.com>
2021-03-18 11:29:13 -0400

    Add support for the SW5e system

--- src/polyglot.js
+++ src/polyglot.js
@@ -15,6 +15,7 @@ class PolyGlot {
 			case "pf1":
 			case "pf2e":
 			case "sfrpg":
+            case "sw5e":
 			case "D35E":
 				return CONFIG[game.system.id.toUpperCase()].languages;
 				break;
@@ -66,6 +67,7 @@ class PolyGlot {
 				return "Common";
 			case "tormenta20":
 				return "Comum";
+            case "sw5e":
 		}
 		const keys = Object.keys(this.languages)
 		if (keys.includes("common") || keys.includes("Common")) return "Common";
@@ -340,6 +342,9 @@ class PolyGlot {
 				break;
 			case "D35E":
 				this.loadLanguages("D35E");
+				break;
+            case "sw5e":
+				this.loadLanguages("sw5e");
 				break;
 			default:
 				break;
