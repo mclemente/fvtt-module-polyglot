d2371065e2d8d002611fb075b461ff8a59a8c19b
mclemente <matheus.xb@gmail.com>
2021-07-28 19:23:11 -0300

    File structure changes
    Just adding `;` to everything.

--- module/api.js
+++ module/api.js
@@ -5,46 +5,46 @@ import {LanguageProvider, ariaLanguageProvider, coc7LanguageProvider,
 	pf1LanguageProvider, pf2eLanguageProvider, sfrpgLanguageProvider, 
 	shadowrun5eLanguageProvider, swadeLanguageProvider, sw5eLanguageProvider,
 	tormenta20LanguageProvider, uesrpgLanguageProvider, warhammerLanguageProvider
-} from "./LanguageProvider.js"
+} from "./LanguageProvider.js";
 
-export const availableLanguageProviders = {}
-export let currentLanguageProvider = undefined
+export const availableLanguageProviders = {};
+export let currentLanguageProvider = undefined;
 
 function register(module, type, languageProvider) {
-	const id = `${type}.${module.id}`
-	let providerInstance = new languageProvider(id)
-	setupProvider(providerInstance)
+	const id = `${type}.${module.id}`;
+	let providerInstance = new languageProvider(id);
+	setupProvider(providerInstance);
 }
 
 function setupProvider(languageProvider) {
-	availableLanguageProviders[languageProvider.id] = languageProvider
-	game.settings.settings.get("polyglot.languageProvider").default = getDefaultLanguageProvider()
-	updateLanguageProvider()
+	availableLanguageProviders[languageProvider.id] = languageProvider;
+	game.settings.settings.get("polyglot.languageProvider").default = getDefaultLanguageProvider();
+	updateLanguageProvider();
 }
 
 export function getDefaultLanguageProvider() {
-	const providerIds = Object.keys(availableLanguageProviders)
+	const providerIds = Object.keys(availableLanguageProviders);
 	if (!providerIds.length) return;
 	// Game systems take the highest precedence for the being the default
-	const gameSystem = providerIds.find(key => key.startsWith("system.") || key.includes(game.system.id))
+	const gameSystem = providerIds.find(key => key.startsWith("system.") || key.includes(game.system.id));
 	if (gameSystem)
-		return gameSystem
+		return gameSystem;
 
 	// If no game system is registered modules are next up.
 	// For lack of a method to select the best module we're just falling back to taking the next best module
 	// settingKeys should always be sorted the same way so this should achive a stable default
-	const module = providerIds.find(key => key.startsWith("module."))
+	const module = providerIds.find(key => key.startsWith("module."));
 	if (module)
-		return module
+		return module;
 
 	// If neither a game system or a module is found fall back to the native implementation
-	return providerIds[0]
+	return providerIds[0];
 }
 
 export function updateLanguageProvider() {
 	// If the configured provider is registered use that one. If not use the default provider
-	const configuredProvider = game.settings.get("polyglot", "languageProvider")
-	currentLanguageProvider = availableLanguageProviders[configuredProvider] ?? availableLanguageProviders[game.settings.settings.get("polyglot.languageProvider").default]
+	const configuredProvider = game.settings.get("polyglot", "languageProvider");
+	currentLanguageProvider = availableLanguageProviders[configuredProvider] ?? availableLanguageProviders[game.settings.settings.get("polyglot.languageProvider").default];
 }
 
 export function initApi() {
@@ -112,22 +112,22 @@ export function initApi() {
 			break;
 	}
 	for (let languageProvider of languageProviders)
-		availableLanguageProviders[languageProvider.id] = languageProvider
-	game.settings.settings.get("polyglot.languageProvider").default = getDefaultLanguageProvider()
-	updateLanguageProvider()
+		availableLanguageProviders[languageProvider.id] = languageProvider;
+	game.settings.settings.get("polyglot.languageProvider").default = getDefaultLanguageProvider();
+	updateLanguageProvider();
 }
 
 export function registerModule(moduleId, languageProvider) {
 	// Check if a module with the given id exists and is currently enabled
-	const module = game.modules.get(moduleId)
+	const module = game.modules.get(moduleId);
 	// If it doesn't the calling module did something wrong. Log a warning and ignore this module
 	if (!module) {
 		console.warn(
 			`Polyglot | A module tried to register with the id "${moduleId}". However no active module with this id was found.` +
 			"This api registration call was ignored. " +
 			"If you are the author of that module please check that the id passed to `registerModule` matches the id in your manifest exactly." +
-			"If this call was made form a game system instead of a module please use `registerSystem` instead.")
-		return
+			"If this call was made form a game system instead of a module please use `registerSystem` instead.");
+		return;
 	}
 	// Using Polyglot's id is not allowed
 	if (moduleId === "polyglot") {
@@ -135,24 +135,24 @@ export function registerModule(moduleId, languageProvider) {
 			`Polyglot | A module tried to register with the id "${moduleId}", which is not allowed. This api registration call was ignored. ` +
 			"If you're the author of the module please use the id of your own module as it's specified in your manifest to register to this api. " +
 			"If this call was made form a game system instead of a module please use `registerSystem` instead."
-		)
-		return
+		);
+		return;
 	}
 
-	register(module, "module", languageProvider)
+	register(module, "module", languageProvider);
 }
 
 export function registerSystem(systemId, languageProvider) {
-	const system = game.system
+	const system = game.system;
 	// If the current system id doesn't match the provided id something went wrong. Log a warning and ignore this module
 	if (system.id != systemId) {
 		console.warn(
 			`Polyglot | A system tried to register with the id "${systemId}". However the active system has a different id.` +
 			"This api registration call was ignored. " +
 			"If you are the author of that system please check that the id passed to `registerSystem` matches the id in your manifest exactly." +
-			"If this call was made form a module instead of a game system please use `registerModule` instead.")
-		return
+			"If this call was made form a module instead of a game system please use `registerModule` instead.");
+		return;
 	}
 
-	register(system, "system", languageProvider)
+	register(system, "system", languageProvider);
 }
--- polyglot.js
+++ polyglot.js
@@ -1,7 +1,7 @@
-import { currentLanguageProvider, initApi, registerModule, registerSystem } from "./module/api.js"
+import { currentLanguageProvider, initApi, registerModule, registerSystem } from "./module/api.js";
 import { LanguageProvider } from "./module/LanguageProvider.js";
 import { Polyglot } from "./module/logic.js";
-import { registerSettings } from "./module/settings.js"
+import { registerSettings } from "./module/settings.js";
 
 Polyglot.FONTS = [
 	"ArCiela",
@@ -51,20 +51,20 @@ Hooks.once("init", () => {
 	initApi();
 });
 
-Hooks.on('renderChatLog', window.polyglot.polyglot.renderChatLog.bind(window.polyglot.polyglot))
-Hooks.on('updateUser', window.polyglot.polyglot.updateUser.bind(window.polyglot.polyglot))
-Hooks.on('controlToken', window.polyglot.polyglot.controlToken.bind(window.polyglot.polyglot))
-Hooks.on('preCreateChatMessage', window.polyglot.polyglot.preCreateChatMessage.bind(window.polyglot.polyglot))
-Hooks.on('renderChatMessage', window.polyglot.polyglot.renderChatMessage.bind(window.polyglot.polyglot))
-Hooks.on('renderJournalSheet', window.polyglot.polyglot.renderJournalSheet.bind(window.polyglot.polyglot))
+Hooks.on('renderChatLog', window.polyglot.polyglot.renderChatLog.bind(window.polyglot.polyglot));
+Hooks.on('updateUser', window.polyglot.polyglot.updateUser.bind(window.polyglot.polyglot));
+Hooks.on('controlToken', window.polyglot.polyglot.controlToken.bind(window.polyglot.polyglot));
+Hooks.on('preCreateChatMessage', window.polyglot.polyglot.preCreateChatMessage.bind(window.polyglot.polyglot));
+Hooks.on('renderChatMessage', window.polyglot.polyglot.renderChatMessage.bind(window.polyglot.polyglot));
+Hooks.on('renderJournalSheet', window.polyglot.polyglot.renderJournalSheet.bind(window.polyglot.polyglot));
 Hooks.on('ready', async () => {
 	await currentLanguageProvider.ready();
 	window.polyglot.polyglot.ready();
 	if (!Object.keys(game.settings.get("polyglot", "Languages")).length) game.settings.set("polyglot", "Languages", currentLanguageProvider.tongues);
-	Hooks.callAll("polyglot.ready", LanguageProvider)
+	Hooks.callAll("polyglot.ready", LanguageProvider);
 });
-Hooks.on("chatBubble", window.polyglot.polyglot.chatBubble.bind(window.polyglot.polyglot)) //token, html, message, {emote}
-Hooks.on("vinoPrepareChatDisplayData", window.polyglot.polyglot.vinoChatRender.bind(window.polyglot.polyglot))
+Hooks.on("chatBubble", window.polyglot.polyglot.chatBubble.bind(window.polyglot.polyglot)); //token, html, message, {emote}
+Hooks.on("vinoPrepareChatDisplayData", window.polyglot.polyglot.vinoChatRender.bind(window.polyglot.polyglot));
 Hooks.on("renderSettingsConfig", (app, html, data) => {
     $('<div>').addClass('form-group polyglot-group-header').html(game.i18n.localize("POLYGLOT.FontSettings")).insertBefore($('[name="polyglot.useUniqueSalt"]').parents('div.form-group:first'));
     $('<div>').addClass('form-group polyglot-group-header').html(game.i18n.localize("POLYGLOT.LanguageSettings")).insertBefore($('[name="polyglot.replaceLanguages"]').parents('div.form-group:first'));
