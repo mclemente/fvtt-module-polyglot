5025de8cb2d3dc243ba1001305844a82075892cd
mclemente <matheus.xb@gmail.com>
2022-02-02 19:05:59 -0300

    Update api.js

--- module/api.js
+++ module/api.js
@@ -131,19 +131,14 @@ export function registerModule(moduleId, languageProvider) {
 	// If it doesn't the calling module did something wrong. Log a warning and ignore this module
 	if (!module) {
 		console.warn(
-			`Polyglot | A module tried to register with the id "${moduleId}". However no active module with this id was found.` +
-				"This api registration call was ignored. " +
-				"If you are the author of that module please check that the id passed to `registerModule` matches the id in your manifest exactly." +
-				"If this call was made form a game system instead of a module please use `registerSystem` instead."
+			`Polyglot | A module tried to register with the id "${moduleId}". However no active module with this id was found. This api registration call was ignored. If you are the author of that module please check that the id passed to "registerModule" matches the id in your manifest exactly. If this call was made form a game system instead of a module please use "registerSystem" instead.`
 		);
 		return;
 	}
 	// Using Polyglot's id is not allowed
 	if (moduleId === "polyglot") {
 		console.warn(
-			`Polyglot | A module tried to register with the id "${moduleId}", which is not allowed. This api registration call was ignored. ` +
-				"If you're the author of the module please use the id of your own module as it's specified in your manifest to register to this api. " +
-				"If this call was made form a game system instead of a module please use `registerSystem` instead."
+			`Polyglot | A module tried to register with the id "${moduleId}", which is not allowed. This api registration call was ignored. If you're the author of the module please use the id of your own module as it's specified in your manifest to register to this api. If this call was made form a game system instead of a module please use "registerSystem" instead.`
 		);
 		return;
 	}
@@ -156,10 +151,7 @@ export function registerSystem(systemId, languageProvider) {
 	// If the current system id doesn't match the provided id something went wrong. Log a warning and ignore this module
 	if (system.id != systemId) {
 		console.warn(
-			`Polyglot | A system tried to register with the id "${systemId}". However the active system has a different id.` +
-				"This api registration call was ignored. " +
-				"If you are the author of that system please check that the id passed to `registerSystem` matches the id in your manifest exactly." +
-				"If this call was made form a module instead of a game system please use `registerModule` instead."
+			`Polyglot | A system tried to register with the id "${systemId}". However the active system has a different id. This api registration call was ignored. If you are the author of that system please check that the id passed to "registerSystem" matches the id in your manifest exactly. If this call was made form a module instead of a game system please use "registerModule" instead.`
 		);
 		return;
 	}
