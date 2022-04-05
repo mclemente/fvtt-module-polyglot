dce9b0230e69b838c7f62e2cfa9f120cf34113e1
Matheus Clemente <matheus.xb@gmail.com>
2022-04-05 12:12:43 -0300

    FilePicker fix

--- module/settings.js
+++ module/settings.js
@@ -64,7 +64,7 @@ export function registerSettings() {
 		hint: "POLYGLOT.directory..hint",
 		default: "",
 		type: String,
-		filePicker: true,
+		filePicker: "folder",
 	});
 	addSetting("source", {
 		name: "POLYGLOT.source.name",
