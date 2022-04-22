2fe55cab6f9c8f01b2f705b5bb2d7fc36b51d7ac
crnormand <crnormand@bellsouth.net>
2022-04-22 16:51:29 -0400

    Update GURPS language provider

--- module/LanguageProvider.js
+++ module/LanguageProvider.js
@@ -1266,17 +1266,57 @@ export class fggLanguageProvider extends LanguageProvider {
 	}
 }
 
+/**
+  Search through all of the advantages (including recursing into containers) looking for "Language" or translation.
+  Depending on the source, it can be two different patterns, Language: NAME (optionals) or Language (NAME) (optionals)
+  and the advantage names may or may not be translated, so we must search for both
+*/
 export class gurpsLanguageProvider extends LanguageProvider {
 	getUserLanguages(actor) {
 		let known_languages = new Set();
 		let literate_languages = new Set();
-		for (let item in actor.data.data.ads) {
-			const match = actor.data.data.ads[item].name.match(/Language \((.+)\)/i);
-			// adding only the descriptive language name, not "Language (XYZ)"
-			if (match) known_languages.add(match[1].trim().toLowerCase());
-		}
-		return [known_languages, literate_languages];
+    if (GURPS) {  // window.GURPS set when the GURPS game system is loaded
+      let npat1 = ": +(?<name>[^\\(]+)"
+      let npat2 = " +\\((?<name>[^\\)]+)\\)"
+      GURPS.recurselist(actor.data.data.ads, (advantage) => {
+        if (!this.updateForPattern(advantage, new RegExp("Language" + npat1, "i"), known_languages, literate_languages))
+          if (!this.updateForPattern(advantage, new RegExp("Language" + npat2, "i"), known_languages, literate_languages))
+            if (!this.updateForPattern(advantage, new RegExp(game.i18n.localize("GURPS.language") + npat1, "i"), known_languages, literate_languages))
+              this.updateForPattern(advantage, new RegExp(game.i18n.localize("GURPS.language") + npat2, "i"), known_languages, literate_languages)
+      })
+    }
+ 		return [known_languages, literate_languages];
 	}
+  
+  /**
+    If we match on the Language name, search the name (or the notes) 
+    for indicators of spoken or written levels of comprehension in English, or translated
+  */
+  updateForPattern(advantage, regex, known_languages, literate_languages) {
+    let match = advantage.name.match(regex)
+    if (match) {
+      const lang = match.groups.name.trim().toLowerCase()
+      const wpat = new RegExp(game.i18n.localize("GURPS.written"), "i")
+      const spat = new RegExp(game.i18n.localize("GURPS.spoken"), "i")
+      let written = advantage.name.match(/written/i) || advantage.notes?.match(/written/i)
+      if (!written) written = advantage.name.match(wpat) || advantage.notes?.match(wpat)
+      let spoken = advantage.name.match(/spoken/i) || advantage.notes?.match(/spoken/i)
+      if (!spoken) spoken = advantage.name.match(spat) || advantage.notes?.match(spat)
+      if (written && spoken) {
+         known_languages.add(lang)
+         literate_languages.add(lang)
+      } else if (written) {
+         literate_languages.add(lang)
+      } else if (spoken) {
+         known_languages.add(lang)
+      } else { // neither is specificaly identified, so assume both
+         known_languages.add(lang)
+         literate_languages.add(lang)
+      }
+      return true 
+    }
+    return false
+  }
 }
 
 export class kryxrpgLanguageProvider extends LanguageProvider {
